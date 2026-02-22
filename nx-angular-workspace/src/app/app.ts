666b5015f3a4b6760f8e10af797315a3fb71debb
thoaivv2 <thoaivv2@gmail.com>
2026-02-22 13:24:07 +0700

    fix error compatibility version and fix bug

--- nx-angular-workspace/src/app/app.ts
+++ nx-angular-workspace/src/app/app.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {SkeletonGridComponent, SkeletonItem } from '../skeleton-grid/skeleton-grid.component';
+import {
+  SkeletonGridComponent,
+  SkeletonItem,
+} from '../skeleton-grid/skeleton-grid.component';
 
 @Component({
   imports: [RouterModule, SkeletonGridComponent],
@@ -29,6 +32,14 @@ export class App {
       type: 'row',
       width: '100%',
       children: [
+        {
+          type: 'column',
+          width: '40%',
+          children: [
+            { type: 'item', width: '100%', height: '142px' }, // Small Top (Right)
+            { type: 'item', width: '100%', height: '142px' }, // Small Bottom (Right)
+          ],
+        },
         { type: 'item', width: '60%', height: '300px' }, // Large Item (Left)
         {
           type: 'column',
